80610765d96512daa7fc5be51867861643528ece
jneilliii <jneilliii+github@gmail.com>
2015-07-11 04:06:27 -0400

    clean up.

--- octoprint_WebcamTempGraph/static/js/webcamtempgraph.js
+++ octoprint_WebcamTempGraph/static/js/webcamtempgraph.js
@@ -2,11 +2,10 @@ $(function () {
 	function webcamtempgraphViewModel(parameters) {
 		var self = this;
 
-		// This will get called before the stlviewerViewModel gets bound to the DOM, but after its depedencies have
-		// already been initialized. It is especially guaranteed that this method gets called _after_ the settings
-		// have been retrieved from the OctoPrint backend and thus the SettingsViewModel been properly populated.
-		self.onAllBound = function () {
-			$("div#temperature-graph").css("background-image","url("+$("img#webcam_image").attr("src")+")");
+		self.onTabChange = function (current, previous) {
+			if (current == "#temperature-graph") {
+				$("div#temperature-graph").css("background-image","url("+$("img#webcam_image").attr("src")+")");
+			}
 		};
 	}
 
