4b93eed556be34b8085d643a8fa9b16dc24aaf5b
jneilliii <jneilliii+github@gmail.com>
2015-07-12 02:37:53 -0400

    jinja variables

--- octoprint_WebcamTempGraph/__init__.py
+++ octoprint_WebcamTempGraph/__init__.py
@@ -2,11 +2,13 @@
 from __future__ import absolute_import
 
 import octoprint.plugin
-import octoprint.settings
 
-class WebcamTempGraph(octoprint.plugin.StartupPlugin, octoprint.plugin.TemplatePlugin):
+class WebcamTempGraph(octoprint.plugin.StartupPlugin, octoprint.plugin.TemplatePlugin, octoprint.plugin.SettingsPlugin):
 	def on_after_startup(self):
-			self._logger.info("WebcamTempGraph loaded!")
+		self._logger.info("WebcamTempGraph loaded!")
+			
+	def get_template_vars(self):
+        return dict(webcamurl=self._settings.get(["webcam","stream"]))
 
 __plugin_name__ = "WebcamTempGraph"
 __plugin_implementation__ = WebcamTempGraph()
