3a00a7d3ef06f84c22c991c6accd2f5372b97587
jneilliii <jneilliii+github@gmail.com>
2019-10-21 22:54:18 -0400

    0.1.1
    - Added Python 3 compatibility

--- octoprint_WebcamTempGraph/__init__.py
+++ octoprint_WebcamTempGraph/__init__.py
@@ -10,6 +10,32 @@ class WebcamTempGraph(octoprint.plugin.StartupPlugin, octoprint.plugin.TemplateP
 			
 	def get_template_vars(self):
 		return dict(webcamurl=octoprint.settings.settings().get(["webcam","stream"]))
-			
+
+	def get_update_information(self):
+		return dict(
+			WebcamTempGraph=dict(
+				displayName="WebcamTempGraph",
+				displayVersion=self._plugin_version,
+
+				# version check: github repository
+				type="github_release",
+				user="jneilliii",
+				repo="OctoPrint-WebcamTempGraph",
+				current=self._plugin_version,
+
+				# update method: pip
+				pip="https://github.com/jneilliii/OctoPrint-WebcamTempGraph/archive/{target_version}.zip"
+			)
+		)
+
 __plugin_name__ = "WebcamTempGraph"
-__plugin_implementation__ = WebcamTempGraph()
+__plugin_pythoncompat__ = ">=2.7,<4"
+
+def __plugin_load__():
+	global __plugin_implementation__
+	__plugin_implementation__ = WebcamTempGraph()
+
+	global __plugin_hooks__
+	__plugin_hooks__ = {
+		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+	}
